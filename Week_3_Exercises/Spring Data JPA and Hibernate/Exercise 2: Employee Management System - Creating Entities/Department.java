61a21f2597e845e4ab3325454b73ceab0dd1a4da
konapalaADiTYA <114147454+konapalaADiTYA@users.noreply.github.com>
2024-08-14 14:21:54 +0530

    Add files via upload

---
+++ Week_3_Exercises/Spring Data JPA and Hibernate/Exercise 2: Employee Management System - Creating Entities/Department.java
@@ -0,0 +1,20 @@
+package com.example.EmployeeManagementSystem;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
---
+++ Week_3_Exercises/Spring Data JPA and Hibernate/Exercise 2: Employee Management System - Creating Entities/Employee.java
@@ -0,0 +1,21 @@
+package com.example.EmployeeManagementSystem;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
