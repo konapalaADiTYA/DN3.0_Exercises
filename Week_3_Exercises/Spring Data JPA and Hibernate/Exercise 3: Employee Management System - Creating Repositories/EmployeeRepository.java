6bae8e07b57876509a9d938ee15275eda0ddd2c0
konapalaADiTYA <114147454+konapalaADiTYA@users.noreply.github.com>
2024-08-14 14:29:08 +0530

    Add files via upload

---
+++ Week_3_Exercises/Spring Data JPA and Hibernate/Exercise 3: Employee Management System - Creating Repositories/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagementSystem;
+
+import com.example.EmployeeManagementSystem.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByDepartmentName(String departmentName);
+
+    Employee findByEmail(String email);
+}
