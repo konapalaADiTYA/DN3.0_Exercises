841bebcd9e4827f4310a8656b464d7111d0a345e
konapalaADiTYA <114147454+konapalaADiTYA@users.noreply.github.com>
2024-08-28 03:50:32 +0530

    Add files via upload

---
+++ Week_5_Exercises/Exercise 10: Online Bookstore - Configuring Content Negotiation/WebConfig.java
@@ -0,0 +1,19 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+            .favorPathExtension(false)
+            .favorParameter(false)
+            .ignoreAcceptHeader(false)
+            .useRegisteredExtensionsOnly(false)
+            .defaultContentType(org.springframework.http.MediaType.APPLICATION_JSON)
+            .mediaType("json", org.springframework.http.MediaType.APPLICATION_JSON)
+            .mediaType("xml", org.springframework.http.MediaType.APPLICATION_XML);
+    }
+}
