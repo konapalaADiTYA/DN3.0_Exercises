b7fadf7c21cfddd9d53c9d15e6a207414f8c466e
konapalaADiTYA <114147454+konapalaADiTYA@users.noreply.github.com>
2024-08-28 15:37:02 +0530

    Add files via upload

---
+++ Week_5_Exercises/Exercise 15: Online Bookstore - API Documentation with Swagger/BookController.java
@@ -0,0 +1,68 @@
+package com.example.BookstoreAPI.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.BookstoreAPI.model.Book;
+import com.example.BookstoreAPI.repository.BookRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> bookOptional = Optional.empty();
+        if (bookOptional.isPresent()) {
+            return ResponseEntity.ok(bookOptional.get());
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Optional<Book> bookOptional = Optional.empty();
+        if (bookOptional.isPresent()) {
+            Book book = bookOptional.get();
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setPrice(bookDetails.getPrice());
+            book.setIsbn(bookDetails.getIsbn());
+            Book updatedBook = bookRepository.save(book);
+            return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        Optional<Book> bookOptional = Optional.empty();
+        if (bookOptional.isPresent()) {
+            bookRepository.delete(bookOptional.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
